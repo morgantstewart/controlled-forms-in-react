5c6288f2850f2a8baef2577c8f2015193df5651d
morgantstewart <morgan@paysigil.com>
2025-08-08 10:40:14 -0400

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,48 @@
 
 import { useState } from 'react';
 
-import './App.css';
-
 const App = () => {
-  const [cityInput, setCityInput] = useState('');
+  const [title, setTitle] = useState('The full name will appear here.');
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+  });
 
   const handleChange = (event) => {
-    setCityInput(event.target.value);
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Set our title state based on the formData state at the time of submission
+    setTitle(`Your name is: ${formData.firstName} ${formData.lastName}`);
+    // Reset fullName state to clear our form inputs
+    setFormData({ firstName: '', lastName: '' });
   };
 
   return (
     <>
-      <label htmlFor="cityInput">City: </label>
-      <input
-        id="cityInput"
-        name="cityInput"
-        type="text"
-        value={cityInput}
-        onChange={handleChange}
-      />
+      <h2>{title}</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="firstName">First Name: </label>
+        <input
+          id="firstName"
+          name="firstName"
+          value={formData.firstName}
+          onChange={handleChange}
+        />
+        <label htmlFor="lastName">Last Name: </label>
+        <input
+          id="lastName"
+          name="lastName"
+          value={formData.lastName}
+          onChange={handleChange}
+        />
+        <button type="submit">Submit your name</button>
+      </form>
     </>
   );
 };
 
 export default App;
+
