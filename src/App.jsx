35aa7cf89a5626f43064b281f511aa2ddce4434b
morgantstewart <morgan@paysigil.com>
2025-08-08 10:32:02 -0400

    completing You Do  on Adding Input Form to App components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-// src/App.jsx
-
-import './App.css'
 
 const App = () => {
+  const [title, setTitle] = useState('The full name will appear here.');
+  const [firstName, setFirstName] = useState('');
+
+   const handleFirstNameChange = (event) => {
+    setFirstName(event.target.value);
+  };
+    const handleLastNameChange = (event) => {
+    setLastName(event.target.value);
+  };
+
+
+
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h2>{title}</h2>
+      <form>
+        <label htmlFor="firstName">First Name: </label>
+        <input
+          id="firstName"
+          value={firstName}
+          <label htmlFor="lastName">Last Name: </label>
+        <input
+          id="lastName"
+          value={lastName}
+        />
+      </form>
+    </>
+
   );
 };
-
-export default App;
